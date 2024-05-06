5ea3a356eccc511c2fc634eb7e4f3218b18e4411
Grauich <118853584+Grauich@users.noreply.github.com>
2024-05-06 19:15:19 +0300

    Update 6lab1.py

--- 6lab1.py
+++ 6lab1.py
@@ -15,7 +15,7 @@ for country, capital in strani.items():
 strana = "Россия"
 print(f"\nСтолица страны {strana}: {strani.get(strana, 'Не найдено')}")
 
-sorted_countries_capitals = sorted(strani.items(), key=lambda x: x[0])
+sorted_countries_capitals = sorted(strani.keys())
 print(f"\nСодержимое словаря в алфавитном порядке:")
 for country, capital in sorted_countries_capitals:
     print(country, "-", capital)
