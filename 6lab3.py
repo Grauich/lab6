a905c686ad116e682180a163f7d9a4daa17d0d21
Grauich <118853584+Grauich@users.noreply.github.com>
2024-05-06 19:34:20 +0300

    Update 6lab3.py

--- 6lab3.py
+++ 6lab3.py
@@ -1,4 +1,4 @@
-allLang = set()
+allLang = set() #В множестве не может быть повторяющихся ключей
 
 studentsLang = {
     "Никита": {"английский", "французский", "немецкий"},
@@ -8,16 +8,18 @@ studentsLang = {
     "Артемыч": {"французский", "немецкий"},
 }
 
-for languages in studentsLang.values():
-    allLang.update(languages)
+for languages in studentsLang.values(): #Идет по значениям (словарям)
+    allLang.update(languages) #Метод множеств (обновляем множества)
 sortedLang = sorted(allLang)
 print("Отсортированный список всех языков:")
 for language in sortedLang:
     print(language)
 
-china = [
-    student for student, languages in studentsLang.items() if "китайский" in languages
-]
+china = []
+
+for student, languages in studentsLang.items()
+    if "китайский" in languages
+        china.append(student)
 
 print("\nСтуденты, которые знают китайский язык:")
 for student in china:
